abe7ed4423450782abf9b00c3ffb0ca390aabe13
임기주 <kijoo1128@yonsei.ac.kr>
2025-10-29 22:31:18 +0900

    feat: pretendard 폰트 적용

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,15 @@ import SignupScreen from "./components/SignupScreen";
 import MainScreen from "./components/MainScreen";
 
 export default function App() {
+  // 🔹 브라우저 창 높이 가져오기
+  const height = window.innerHeight;
+
   return (
-    <div className="flex justify-center items-center min-h-screen bg-white">
+    <div
+      className="flex justify-center items-center bg-white"
+      // 🔹 style로 직접 높이 지정
+      style={{ height: `${height}px` }}
+    >
       <div className="w-[23.44rem] h-[45.88rem] bg-white shadow-md overflow-hidden border border-gray-200">
         <Router>
           <Routes>
