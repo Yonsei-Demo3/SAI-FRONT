03decccc785499e6090d2b5c09cce9847102b943
Hoyoung027 <hybyun0207@gmail.com>
2025-11-25 01:57:15 +0900

    feat : 채팅방 리스트 api 연결

--- src/lib/chatService.js
+++ src/lib/chatService.js
@@ -1,5 +1,16 @@
 import axiosInstance from "./axiosInstance";
 
-export async function getChatList() {
-  return axiosInstance.get("/api/v1/questions");  
+export async function getChatList(sort) {
+  return axiosInstance.get("/api/v1/questions", {
+    params: { 
+      "sort": sort },
+  });  
+}
+
+export async function readyChat(questionId) {
+  return axiosInstance.post(`/api/v1/questions/ready/${questionId}`);  
+}
+
+export async function quitChat(questionId) {
+  return axiosInstance.delete(`/api/v1/questions/${questionId}`);  
 }
